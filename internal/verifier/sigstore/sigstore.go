101e286916cd58d288e4bc63d9a5a3637c2e5512
Jakub Hrozek <jakub@stacklok.com>
2024-02-05 17:21:10 +0100

    WIP

--- internal/verifier/sigstore/sigstore.go
+++ internal/verifier/sigstore/sigstore.go
@@ -17,8 +17,14 @@ package sigstore
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"io"
+	"io/fs"
+	"net/http"
+	"net/url"
 	"os"
+	"path"
 	"strings"
 
 	"github.com/rs/zerolog/log"
@@ -35,6 +41,11 @@ const (
 	SigstorePublicTrustedRootRepo = "tuf-repo-cdn.sigstore.dev"
 	// LocalCacheDir is the local cache directory for the verifier
 	LocalCacheDir = "/tmp/minder-cache"
+)
+
+const (
+	newFilePerms = os.FileMode(0600)
+	newDirPerms  = os.FileMode(0750)
 )
 
 // Sigstore is the sigstore verifier
@@ -54,6 +65,10 @@ func New(trustedRoot string, authOpts ...container.AuthMethod) (*Sigstore, error
 	}
 
 	// init sigstore's verifier
+	if err := seedRootJson(trustedRoot, cacheDir); err != nil {
+		return nil, err
+	}
+
 	trustedrootJSON, err := tuf.GetTrustedrootJSON(trustedRoot, cacheDir)
 	if err != nil {
 		return nil, err
@@ -62,8 +77,11 @@ func New(trustedRoot string, authOpts ...container.AuthMethod) (*Sigstore, error
 	if err != nil {
 		return nil, err
 	}
-	sev, err := verify.NewSignedEntityVerifier(trustedMaterial, verify.WithSignedCertificateTimestamps(1),
-		verify.WithTransparencyLog(1), verify.WithObserverTimestamps(1))
+	/*
+		sev, err := verify.NewSignedEntityVerifier(trustedMaterial, verify.WithSignedCertificateTimestamps(1),
+			verify.WithTransparencyLog(1), verify.WithObserverTimestamps(1))
+	*/
+	sev, err := verify.NewSignedEntityVerifier(trustedMaterial, verify.WithSignedTimestamps(1))
 	if err != nil {
 		return nil, err
 	}
@@ -108,14 +126,14 @@ func (s *Sigstore) ClearCache() {
 	}
 }
 
-func createTmpDir(path, prefix string) (string, error) {
+func createTmpDir(basePath, prefix string) (string, error) {
 	// ensure the path exists
-	err := os.MkdirAll(path, os.ModePerm)
+	err := os.MkdirAll(basePath, os.ModePerm)
 	if err != nil {
 		return "", fmt.Errorf("failed to ensure path for temporary sigstore cache directory: %w", err)
 	}
 	// create the temporary directory
-	tmpDir, err := os.MkdirTemp(path, prefix)
+	tmpDir, err := os.MkdirTemp(basePath, prefix)
 	if err != nil {
 		return "", fmt.Errorf("failed to create temporary sigstore cache directory: %w", err)
 	}
@@ -130,4 +148,112 @@ func sanitizeInput(registry *verifyif.ArtifactRegistry, owner *string) {
 	}
 	// (jaosorior): The owner can't be upper-cased, normalize the owner.
 	*owner = strings.ToLower(*owner)
+}
+
+func seedRootJson(tufRepo, cacheDir string) error {
+	// sigstore-go has a copy of the root.json for the public sigstore
+	// instance embedded. Nothing to do.
+	if tufRepo == SigstorePublicTrustedRootRepo {
+		return nil
+	}
+
+	rootJson, err := getRootJson(tufRepo)
+	if err != nil {
+		return err
+	}
+
+	return writeRootJson(tufRepo, cacheDir, rootJson)
+}
+
+func getRootJson(tufRepo string) ([]byte, error) {
+	tufRoot, err := normalizeUrl(tufRepo)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	resp, err := http.Get(tufRoot) // #nosec G107 - this URL is user-provided and affects only evaluation in that single project
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	return io.ReadAll(resp.Body)
+}
+
+func normalizeUrl(tufRoot string) (string, error) {
+	if !strings.Contains(tufRoot, "://") {
+		// this is likely a naive way of checking for the schema
+		tufRoot = "https://" + tufRoot
+	}
+
+	parsedUrl, err := url.Parse(tufRoot)
+	if err != nil {
+		return "", err
+	}
+
+	if parsedUrl.Scheme == "" {
+		parsedUrl.Scheme = "https"
+	} else if parsedUrl.Scheme != "https" {
+		return "", fmt.Errorf("unsupported scheme: %s", parsedUrl.Scheme)
+	}
+
+	if strings.HasSuffix(parsedUrl.Path, "../") {
+		return "", errors.New("invalid path")
+	}
+
+	if parsedUrl.RawQuery != "" || parsedUrl.Fragment != "" {
+		return "", errors.New("invalid query or fragment")
+	}
+
+	if parsedUrl.Path == "" {
+		parsedUrl.Path = "/1.root.json"
+	}
+
+	return parsedUrl.String(), nil
+}
+
+func writeRootJson(tufRepo, cacheDir string, rootJson []byte) error {
+	tufPath := path.Join(cacheDir, tufRepo)
+	fi, err := os.Stat(tufPath)
+	if errors.Is(err, fs.ErrNotExist) {
+		if err = os.MkdirAll(tufPath, newDirPerms); err != nil {
+			return fmt.Errorf("error creating directory for metadata cache: %w", err)
+		}
+	} else if err != nil {
+		return fmt.Errorf("error getting FileInfo for %s: %w", tufPath, err)
+	} else {
+		if !fi.IsDir() {
+			return fmt.Errorf("can not open %s, not a directory", tufPath)
+		}
+		// Verify file mode is not too permissive.
+		if err = ensureMaxPermissions(fi, newDirPerms); err != nil {
+			return err
+		}
+	}
+
+	rootPath := path.Join(tufPath, tuf.RootTUFPath)
+	return os.WriteFile(rootPath, rootJson, newFilePerms)
+}
+
+// taken from go-tuf/internal/fsutil/perm.go
+//
+// EnsureMaxPermissions tests the provided file info, returning an error if the
+// file's permission bits contain excess permissions not set in maxPerms.
+//
+// For example, a file with permissions -rw------- will successfully validate
+// with maxPerms -rw-r--r-- or -rw-rw-r--, but will not validate with maxPerms
+// -r-------- (due to excess --w------- permission) or --w------- (due to
+// excess -r-------- permission).
+//
+// Only permission bits of the file modes are considered.
+func ensureMaxPermissions(fi os.FileInfo, maxPerms os.FileMode) error {
+	gotPerm := fi.Mode().Perm()
+	forbiddenPerms := (^maxPerms).Perm()
+	excessPerms := gotPerm & forbiddenPerms
+
+	if excessPerms != 0 {
+		return fmt.Errorf("permission bits for file %v failed validation: want at most %v, got %v with excess perms %v", fi.Name(), maxPerms.Perm(), gotPerm, excessPerms)
+	}
+
+	return nil
 }
